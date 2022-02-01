5c88888b8518cf51ea3b28983251a59b31f7bc13
maricdjurdjica <maric.djurdjica@gmail.com>
2022-02-01 22:47:26 +0100

    Application Firewall logovanje 2

--- SBESProjectSolution/ApplicationFirewall/Program.cs
+++ SBESProjectSolution/ApplicationFirewall/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Contracts;
 
 namespace ApplicationFirewall
 {
@@ -10,6 +11,56 @@ namespace ApplicationFirewall
     {
         static void Main(string[] args)
         {
+            WhitelistConfig wc = new WhitelistConfig();
+            Event ev;
+
+            while (true)
+            {
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("\t1. Add port");
+                Console.WriteLine("\t2. Remove port");
+                Console.WriteLine("\t3. Add protocol");
+                Console.WriteLine("\t4. Remove protocol");
+
+                string x = Console.ReadLine();
+                string input;
+                bool val;
+
+                switch (x)
+                {
+                    case "1":
+                        Console.Write("Port: ");
+                        input = Console.ReadLine();
+                        val = wc.AddPort(Int32.Parse(input), out ev);
+                        if (!val)
+                            Console.WriteLine("Port already exists.");
+                        break;
+                    case "2":
+                        Console.Write("Port: ");
+                        input = Console.ReadLine();
+                        val = wc.RemovePort(Int32.Parse(input), out ev);
+                        if (!val)
+                            Console.WriteLine("Port doesn't exist.");
+                        break;
+                    case "3":
+                        Console.Write("Protocol: ");
+                        input = Console.ReadLine();
+                        val = wc.AddProtocol(input, out ev);
+                        if (!val)
+                            Console.WriteLine("Protocol already exists.");
+                        break;
+                    case "4":
+                        Console.Write("Protocol: ");
+                        input = Console.ReadLine();
+                        val = wc.RemoveProtocol(input, out ev);
+                        if (!val)
+                            Console.WriteLine("Protocol doesn't exist.");
+                        break;
+                    default:
+                        Console.WriteLine("Please enter 1, 2, 3 or 4.");
+                        break;
+                }
+            }
         }
     }
 }
--- SBESProjectSolution/ApplicationFirewall/WhitelistConfig.cs
+++ SBESProjectSolution/ApplicationFirewall/WhitelistConfig.cs
@@ -1,52 +1,133 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using AFManager;
+using Contracts;
 
 namespace ApplicationFirewall
 {
     public class WhitelistConfig
     {
-        List<int> validPorts = new List<int>();
-        List<string> validProtocols = new List<string>();
+        internal static List<int> validPorts = new List<int>();
+        internal static List<string> validProtocols = new List<string>();
+        private static int key;
 
-        public bool AddPort(int port)
+        static WhitelistConfig()
         {
+            key = 0;
+
+            validPorts.Add(9999);
+            validPorts.Add(9998);
+            validPorts.Add(9997);
+
+            validProtocols.Add("TCP");
+            validProtocols.Add("UDP");
+        }
+
+        public bool AddPort(int port, out Event ev)
+        {
+            ev = null;
+
             if (!validPorts.Contains(port))
             {
                 validPorts.Add(port);
-                return true;
+                ev = new Event(++key, CriticallityLevel.GREEN_ALERT, DateTime.Now, null, "AddPort", MessageState.OPEN);
+
+                try
+                {
+                    Audit.AddPortSuccess(port.ToString());
+                    return true;
+                }
+                catch (FaultException<SecurityException> e)
+                {
+                    Console.WriteLine(e.Detail.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             return false;
         }
 
-        public bool RemovePort(int port)
+        public bool RemovePort(int port, out Event ev)
         {
+            ev = null;
             if (validPorts.Contains(port))
             {
                 validPorts.Remove(port);
-                return true;
+                ev = new Event(++key, CriticallityLevel.YELLOW_ALERT, DateTime.Now, null, "RemovePort", MessageState.CLOSE);
+
+                try
+                {
+                    Audit.RemovePortSuccess(port.ToString());
+                    return true;
+                }
+                catch (FaultException<SecurityException> e)
+                {
+                    Console.WriteLine(e.Detail.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             return false;
         }
 
-        public bool AddProtocol(string protocol)
+        public bool AddProtocol(string protocol, out Event ev)
         {
+            ev = null;
+            protocol = protocol.ToUpper();
+
             if (!validProtocols.Contains(protocol))
             {
                 validProtocols.Add(protocol);
-                return true;
+                ev = new Event(++key, CriticallityLevel.GREEN_ALERT, DateTime.Now, null, "AddProtocol", MessageState.OPEN);
+
+                try
+                {
+                    Audit.AddProtocolSuccess(protocol);
+                    return true;
+                }
+                catch (FaultException<SecurityException> e)
+                {
+                    Console.WriteLine(e.Detail.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             return false;
         }
 
-        public bool RemoveProtocol(string protocol)
+        public bool RemoveProtocol(string protocol, out Event ev)
         {
+            ev = null;
+            protocol = protocol.ToUpper();
+
             if (validProtocols.Contains(protocol))
             {
                 validProtocols.Remove(protocol);
-                return true;
+                ev = new Event(++key, CriticallityLevel.RED_ALERT, DateTime.Now, null, "RemoveProtocol", MessageState.CLOSE);
+
+                try
+                {
+                    Audit.RemoveProtocolSuccess(protocol);
+                    return true;
+                }
+                catch (FaultException<SecurityException> e)
+                {
+                    Console.WriteLine(e.Detail.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
             return false;
         }
